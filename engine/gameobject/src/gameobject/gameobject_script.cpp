b4c1a1b3272a302f2495d4230e19feb993979a0c
redoak <22959728+redoak@users.noreply.github.com>
2024-11-12 21:01:21 +0100

    Switched to using lua_tolstring and luaL_checklstring where length is needed

--- engine/gameobject/src/gameobject/gameobject_script.cpp
+++ engine/gameobject/src/gameobject/gameobject_script.cpp
@@ -331,11 +331,12 @@ namespace dmGameObject
     static int ScriptInstanceResolvePath(lua_State* L)
     {
         ScriptInstance* i = (ScriptInstance*)lua_touserdata(L, 1);
-        const char* path = luaL_checkstring(L, 2);
+        size_t path_length;
+        const char* path = luaL_checklstring(L, 2, &path_length);
 
-        if (path != 0x0 && *path != 0)
+        if (path != 0x0 && path_length != 0)
         {
-            dmScript::PushHash(L, GetAbsoluteIdentifier(i->m_Instance, path, strlen(path)));
+            dmScript::PushHash(L, GetAbsoluteIdentifier(i->m_Instance, path, path_length));
         }
         else
         {
@@ -1444,8 +1445,9 @@ namespace dmGameObject
         ScriptInstance* i = ScriptInstance_Check(L);
         if (lua_gettop(L) > 0)
         {
-            const char* ident = luaL_checkstring(L, 1);
-            dmScript::PushHash(L, GetAbsoluteIdentifier(i->m_Instance, ident, strlen(ident)));
+            size_t length;
+            const char* ident = luaL_checklstring(L, 1, &length);
+            dmScript::PushHash(L, GetAbsoluteIdentifier(i->m_Instance, ident, length));
         }
         else
         {
